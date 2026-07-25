e58eac6a39df8c62885e26d82769b9af2423bb22
AsifAliSabbir <asifalisabbir@gmail.com>
2026-07-26 01:59:43 +0400

    v27.07

--- AbayaSystem.Web/GlobalFunctions.cs
+++ AbayaSystem.Web/GlobalFunctions.cs
@@ -6,14 +6,14 @@ namespace AbayaSystem.Web
     {
 
 
-        ////Local
-        public static string conl = @"Server=localhost;Database=AbayaBoutiqueDb;Integrated Security=True;TrustServerCertificate=True;";
+        //////Local
+        //public static string conl = @"Server=localhost;Database=AbayaBoutiqueDb;Integrated Security=True;TrustServerCertificate=True;";
 
         ////somee
         //public static string conl = @"workstation id=AbayaBoutiqueDb.mssql.somee.com;packet size=4096;user id=asifalisabbir_SQLLogin_1;pwd=o61shj57nu;data source=AbayaBoutiqueDb.mssql.somee.com;persist security info=False;initial catalog=AbayaBoutiqueDb;TrustServerCertificate=True;";
 
-        ////monsterasp
-        //public static string conl = @"Server=db59869.databaseasp.net; Database=db59869; User Id=db59869; Password=Cy8+?Ga3h%2T; Encrypt=False; MultipleActiveResultSets=True;";
+        //monsterasp
+        public static string conl = @"Server=db59869.databaseasp.net; Database=db59869; User Id=db59869; Password=Cy8+?Ga3h%2T; Encrypt=False; MultipleActiveResultSets=True;";
 
 
 
